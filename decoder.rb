6e48b9fd0f1984ccea322d94c4b0c1aa3bc7076e
Robert Horrigan <roberthorrigan@gmail.com>
2014-05-02 15:52:04 -0400

    Complete String Manipulation

--- decoder.rb
+++ decoder.rb
@@ -1,1 +1,11 @@
 # your code goes here
+def decode(string)
+  empty_string = ""
+
+
+  string.each_byte do |character|
+    new_value = character -1
+    empty_string += new_value.chr
+  end
+  empty_string    
+end  
--- encoder.rb
+++ encoder.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def encode(string)
+  empty_string = ""
+
+  string.each_byte do |character|
+    new_value = character.ord + 1
+    empty_string += new_value.chr
+  end
+  
+  empty_string  
+end  
